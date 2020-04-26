4ae19d72f16a1f10e255686c75f09f2b8e8c944c
Prakash <prakasamonline@gmail.com>
2020-04-27 00:23:10 +0530

    [NEW] Cors package added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet());
+  app.enableCors();
+  await app.listen(4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
